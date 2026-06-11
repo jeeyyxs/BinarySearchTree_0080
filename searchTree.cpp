5c85764bb4400ebfd3cda17acf26575b194fcbcb
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-06-11 17:52:41 +0700

    membuat class node

--- searchTree.cpp
+++ searchTree.cpp
@@ -1,3 +1,13 @@
-#include <iostream>
+#include <iostream> //library
 #include <string>
 using namespace std;
+
+class Node
+{
+    public:
+    string info;
+    Node* leftchild;
+    Node* rigtchild;
+
+}
+
